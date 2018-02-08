e7f7049078747a547b0529c05081c9fa1dec27be
Rafizan Uda Baharum <rafizan.baharum@canang.com.my>
2018-02-08 08:21:15 +0800

    Remove sensitive data

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,15 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const environment: { production: boolean, endpoint: string; firebaseConfig: any } = {
+export const environment: { production: boolean, endpoint: string } = {
   production: false,
   endpoint: '/data',
-  firebaseConfig: {
-    apiKey: 'AIzaSyCj_F5O6tboRNQTvgkLfo10LFPzoahWPag',
-    authDomain: 'kahwin2-6ca62.firebaseapp.com',
-    databaseURL: 'https://kahwin2-6ca62.firebaseio.com',
-    projectId: 'kahwin2-6ca62',
-    storageBucket: 'kahwin2-6ca62.appspot.com',
-    messagingSenderId: '542374893569',
-  },
 };
